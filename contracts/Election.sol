b003b6d7d6baf36005a81d2fb3449e5e729fdea1
sayakghosh26 <sayak.ghosh26@gmail.com>
2022-12-11 18:06:26 +0530

    2-list-candidates

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -2,11 +2,24 @@
 pragma solidity >=0.5.0;
 
 contract Election {
+    //Model a Candidate
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
     //Store Candidate
-    //Read Candidate
-    string public candidate;
+    //Fetch Candidate
+    mapping(uint => Candidate) public candidates;
+    //Store Candidates Count
+    uint public candidatesCount;
     //Constructor
     constructor() public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+    function addCandidate(string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
