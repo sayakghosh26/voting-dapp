df2f99282e9cb011fc2ce0469071b434c4086410
sayakghosh26 <sayak.ghosh26@gmail.com>
2022-12-11 18:55:19 +0530

    3-cast-votes

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -8,6 +8,8 @@ contract Election {
         string name;
         uint voteCount;
     }
+    //Store accounts that have voted
+    mapping(address => bool) public voters;
     //Store Candidate
     //Fetch Candidate
     mapping(uint => Candidate) public candidates;
@@ -21,5 +23,16 @@ contract Election {
     function addCandidate(string memory _name) private {
         candidatesCount ++;
         candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote (uint _candidateId) public {
+        //Require that they haven't voted before
+        require(!voters[msg.sender]);
+        //Require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+        //Record that voter has voted
+        voters[msg.sender] = true;
+        //Update candidate vote count
+        candidates[_candidateId].voteCount ++;
     }
 }
